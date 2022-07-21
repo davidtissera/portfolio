6d4f389cd91fa52c1b7daaa049fd548fec520fa4
David Alejandro Tissera <davidtissera94@gmail.com>
2022-07-21 02:28:33 -0300

    Compensate background height with margin

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import Layout from './components/Layout/Layout'
 import Router from './router/Router'
 import { BrowserRouter } from 'react-router-dom'
 import LoadingDotFlashing from '@/components/LoadingDotFlashing'
+import { useMobile } from './hooks/useMobile'
 
 function App() {
   const [isLoading, setLoading] = useState(true)
+  const isMobile = useMobile()
+  const sidebarHeightMobile = "60px"
+  const sidebarHeightDesktop = "120px"
 
   useEffect(() => {
     setTimeout(() => {
@@ -23,7 +27,9 @@ function App() {
     <div>
       <BrowserRouter>
         <Layout />
-        <Router />
+        <div style={{ marginTop: isMobile ? sidebarHeightMobile : sidebarHeightDesktop }}>
+          <Router />
+        </div>
       </BrowserRouter>
     </div>
   )
