d90998c8310a46aa2f437c2a35116c51041f7ef8
David Alejandro Tissera <davidtissera94@gmail.com>
2022-07-22 16:19:03 -0300

    Adding working experiences

---
+++ src/components/WorkExperience.tsx
@@ -0,0 +1,21 @@
+export interface IWorkExperience {
+  title: string;
+  type: string;
+  activityDate: string;
+  about: string;
+  technologiesInvolved: string[];
+}
+
+export default function WorkExperience(props: IWorkExperience) {
+  const { title, activityDate, about, technologiesInvolved, type } = props
+
+  return (
+    <>
+      <span className="badge text-bg-primary">{activityDate}</span>
+      <span className="badge text-bg-secondary mx-2">{type}</span>
+      <h2 className="fw-bolder mt-1 mb-0">{title}</h2>
+      <p className="fs-6 text-muted mt-3 mb-2">{about}</p>
+      <small className="text-decoration-underline">Technologies used:</small><small> {technologiesInvolved.join(" - ")}</small>
+    </>
+  )
+}
--- src/views/Resume/Resume.tsx
+++ src/views/Resume/Resume.tsx
@@ -1,84 +1,33 @@
-import FlippingCard from "@/components/FlippingCard"
+import WorkExperience from '@/components/WorkExperience'
 import workExperiences from './work-experiences.json'
 import './css/Resume.scss'
 
 export default function Resume() {
   return (
     <div className="container mt-4">
-      <h1 className="fw-bold text-center text-md-start">Work experiences</h1>
-      <div className="row row-cols-md-2 row-cols-lg-3">
-        {workExperiences.map((workExperience, idx) => {
-          const {
-            name,
-            logo,
-            activity_date: activityDate,
-            website,
-            about,
-            technologies_involved: technologiesInvolved,
-            work_methodologies: workMethodologies
-          } = workExperience
+      <h1 className="fw-bold text-center text-md-start mb-4">Work experiences</h1>
+      <div>
+        {workExperiences.map(({
+          name,
+          activity_date: activityDate,
+          about,
+          type,
+          technologies_involved: technologiesInvolved
+        }) => {
           return (
-            <div className="col my-3 d-flex align-items-center justify-content-center">
-              <FlippingCard
+            <div className="my-5">
+              <WorkExperience
                 key={name}
-                front={<ResumeFront companyLogo={logo} />}
-                back={(
-                  <ResumeBack
-                    name={name}
-                    activityDate={activityDate}
-                    technologiesInvolved={technologiesInvolved}
-                    workMethodologies={workMethodologies}
-                    website={website}
-                    about={about}
-                  />
-                )}
+                title={name}
+                type={type}
+                activityDate={activityDate}
+                about={about}
+                technologiesInvolved={technologiesInvolved}
               />
             </div>
           )
         })}
       </div>
-    </div>
-  )
-}
-
-function ResumeFront(props: any) {
-  const { companyLogo } = props
-  return (
-    <img className="resume-front-img" src={companyLogo} />
-  )
-}
-
-function ResumeBack(props: any) {
-  const {
-    name,
-    activityDate,
-    about,
-    technologiesInvolved,
-    workMethodologies
-  } = props
-
-  const keyValuePairs = [
-    { label: "Activity Date", value: activityDate },
-    { label: "About", value: about },
-    { label: "Technologies Involved", value: technologiesInvolved.join(" - ") },
-    { label: "Work Methodologies", value: workMethodologies.join(" - ") },
-  ]
-
-  return (
-    <div className="resume-back container p-4">
-      <h2 className="mb-3">{name}</h2>
-      {keyValuePairs.map((keyValuePair) => {
-        return (
-          <div key={keyValuePair.value}>
-            <div className="key">
-              {keyValuePair.label}
-            </div>
-            <p className="value">
-              {keyValuePair.value}
-            </p>
-          </div>
-        )
-      })}
     </div>
   )
 }
